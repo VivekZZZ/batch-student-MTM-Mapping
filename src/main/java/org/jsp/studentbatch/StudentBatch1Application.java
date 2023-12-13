6c01adc7d84f301b612fb797016a70405e505d03
vivek kumar <86564710+VivekZZZ@users.noreply.github.com>
2023-12-13 10:43:58 +0530

    first commit

---
+++ src/main/java/org/jsp/studentbatch/StudentBatch1Application.java
@@ -0,0 +1,13 @@
+package org.jsp.studentbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentBatch1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentBatch1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/jsp/studentbatch/StudentBatch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.studentbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentBatch1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
